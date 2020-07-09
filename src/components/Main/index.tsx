e3daeda102fbd530abfc9ba3fd28428a0cff3304
leoalcantara <leonidio.ti@gmail.com>
2020-07-09 14:31:11 -0300

    styling the ProfilePage component

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import ProfilePage from '../ProfilePage';
+
 import { 
   Container, 
   Header, 
@@ -25,7 +27,9 @@ const Main: React.FC = () => {
           <span>100 Tweets</span>
         </ProfileInfo>
       </Header>
-      {/* </ProfilePage> */}
+
+      <ProfilePage/>
+      
       <BottonMenu>
         <HomeIcon className="active"/>
         <SearchIcon/>
---
+++ src/components/ProfilePage/index.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+
+import { 
+  Container,
+  Banner,
+  Avatar, 
+  ProfileData, 
+  LocationIcon, 
+  CakeIcon, 
+  Followage 
+} from './styles';
+
+const ProfilePage: React.FC = () => {
+  return(
+    <Container>
+      <Banner>
+        <Avatar/>        
+      </Banner>
+
+      <ProfileData>
+        {/* <EditButton outlined>Editar perfil</EditButton> */}
+        <h1>Leonidio Alcantara</h1>
+        <h2>@leoalcantara</h2>
+        <p>
+          Developer at <a href="#">@Software</a>
+        </p>
+
+        <ul>
+          <li>
+            <LocationIcon />
+            Itaperuna-RJ, Brasil
+          </li>
+          <li>
+            <CakeIcon />
+            Nascido(a) em 16 de outubro
+          </li>          
+        </ul>
+
+        <Followage>
+          <span>
+            seguindo <strong>76</strong>
+          </span>
+          <span>
+            <strong>11 </strong> seguidores
+          </span>
+        </Followage>
+      </ProfileData>
+    </Container>
+  );
+}
+
+export default ProfilePage;
