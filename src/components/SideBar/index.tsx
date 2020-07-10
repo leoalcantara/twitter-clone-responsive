e529665fc61180422d843ea3554790624a6127e8
leoalcantara <leonidio.ti@gmail.com>
2020-07-10 15:46:46 -0300

    importing the StickBox library

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 
+import StickBox from 'react-sticky-box';
+
+
 import List from '../List';
 import FollowSuggestion from '../FollowSuggestion';
 import News from '../News';
@@ -20,30 +23,39 @@ const SideBar: React.FC = () => {
         <SearchInput placeholder="Buscar no Twitter" />
         <SearchIcon />
       </SearchWrapper>
-
-      <Body>
-        <List 
-          title = "Talvez você curta"
-          elements = {[
-            <FollowSuggestion
-              name="Luiz Batanero"
-              nickname="@luizbatanero"
-            />,
-            <FollowSuggestion 
-              name="Luke Morales" 
-              nickname="@lukemorales"                
-            />,
-            <FollowSuggestion
-              name="Camila Magalhães"
-              nickname="@camilaamgl"
-            />,
-          ]}
-        /> 
-        <List 
-          title = "Talvez você curta"
-          elements = {[<News/>, <News/>, <News/>]}
-        />       
-      </Body>
+      <StickBox>
+        <Body>
+          <List 
+            title = "Talvez você curta"
+            elements = {[
+              <FollowSuggestion
+                name="Luiz Batanero"
+                nickname="@luizbatanero"
+              />,
+              <FollowSuggestion 
+                name="Luke Morales" 
+                nickname="@lukemorales"                
+              />,
+              <FollowSuggestion
+                name="Camila Magalhães"
+                nickname="@camilaamgl"
+              />,
+            ]}
+          /> 
+          <List 
+            title = "Talvez você curta"
+            elements = {[<News/>, <News/>, <News/>]}
+          />
+          <List 
+            title = "Talvez você curta"
+            elements = {[<News/>, <News/>, <News/>]}
+          />
+          <List 
+            title = "Talvez você curta"
+            elements = {[<News/>, <News/>, <News/>]}
+          />         
+        </Body>
+      </StickBox>  
     </Container>
   );
 }
